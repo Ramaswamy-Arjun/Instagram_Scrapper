adc1f070253b9d1ae2e7a7bc5bddec48467650b4
Ramaswamy-Arjun <116621872+Ramaswamy-Arjun@users.noreply.github.com>
2023-09-13 18:51:11 +0530

    Add files via upload

---
+++ Instagram_Scraper.py
@@ -0,0 +1,338 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+from bs4 import BeautifulSoup
+import time
+import logging
+from tqdm import tqdm
+import pandas as pd
+import argparse
+import os
+import wget
+
+
+logger = logging.getLogger('InstaBOT')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = \
+    logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+class ImageScraper():
+    def __init__(self, username, password):
+        self.__driver = webdriver.Chrome()
+        self.__driver.get("http://www.instagram.com")
+        self.__login(username, password)
+        
+    def __login(self, username_ig, password_ig):
+        #Wait until the element be clickable => load it in the webpage
+        username = WebDriverWait(self.__driver, 10).\
+            until(EC.element_to_be_clickable\
+                ((By.CSS_SELECTOR, "input[name='username']")))
+
+        password = WebDriverWait(self.__driver, 10).\
+            until(EC.element_to_be_clickable\
+                ((By.CSS_SELECTOR, "input[name='password']")))
+
+        #Cleaning the fields
+        username.clear()
+        username.send_keys(username_ig)
+        password.clear()
+        password.send_keys(password_ig)
+
+        #Login
+        login_button = WebDriverWait(self.__driver, 10)\
+            .until(EC.element_to_be_clickable\
+                ((By.CSS_SELECTOR, "button[type='submit']"))).click()
+
+        #Skipping not now
+        not_now = WebDriverWait(self.__driver, 10).\
+            until(EC.element_to_be_clickable\
+                ((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+        #Use Only when necessary, otherwise causes error.
+        '''
+        not_now2 = WebDriverWait(self.__driver, 10).\
+            until(EC.element_to_be_clickable\
+                ((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+        '''        
+    def scrape(self, keyword):
+        self.__driver.get("http://www.instagram.com/explore/tags/" + keyword + "/")
+        # Wait for 5 seconds
+        time.sleep(5)
+
+        #scroll down to scrape more images
+        self.__driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        #target all images on the page
+        images = self.__driver.find_elements(By.TAG_NAME, 'img')
+        images = [image.get_attribute('src') for image in images]
+        images = images[:-2]
+
+        print('Number of scraped images: ', len(images))
+
+        path = os.getcwd()
+        path = os.path.join(path, keyword + "s")
+
+        #create the directory
+        os.mkdir(path)
+
+        path
+
+        #download images
+        counter = 0
+        for image in images[3:]:
+            save_as = os.path.join(path, keyword + str(counter) + '.jpg')
+            wget.download(image, save_as)
+            counter += 1  
+        print("Images Saved Successfully!!!")
+
+    
+
+class InstaScraper():
+    def __init__(self, username, password):
+        self.__driver = webdriver.Chrome()
+        self.__driver.get("http://www.instagram.com")
+        self.__login(username, password)
+
+    def __login(self, username_ig, password_ig):
+        #Wait until the element be clickable => load it in the webpage
+        username = WebDriverWait(self.__driver, 10).\
+            until(EC.element_to_be_clickable\
+                ((By.CSS_SELECTOR, "input[name='username']")))
+
+        password = WebDriverWait(self.__driver, 10).\
+            until(EC.element_to_be_clickable\
+                ((By.CSS_SELECTOR, "input[name='password']")))
+
+        #Cleaning the fields
+        username.clear()
+        username.send_keys(username_ig)
+        password.clear()
+        password.send_keys(password_ig)
+
+        #Login
+        login_button = WebDriverWait(self.__driver, 10)\
+            .until(EC.element_to_be_clickable\
+                ((By.CSS_SELECTOR, "button[type='submit']"))).click()
+
+        #Skipping not now
+        not_now = WebDriverWait(self.__driver, 10).\
+            until(EC.element_to_be_clickable\
+                ((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+        #Use Only when necessary, otherwise causes error.
+        '''
+        not_now2 = WebDriverWait(self.__driver, 10).\
+            until(EC.element_to_be_clickable\
+                ((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+        '''        
+
+    def search(self, keyword):
+        self.__driver.get("http://www.instagram.com/explore/tags/" + keyword + "/")
+        
+    
+    def __lcondition(self, link):
+        return '.com/p/' in link.get_attribute('href')
+
+    def __get_user(self):
+        xpath1 = '//div[contains(@id, "mount_0_0_")]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section\
+              /main/div/div[1]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div[1]/div/span/div/div/a'
+        xpath2 = '//div[contains(@id, "mount_0_0_")]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section\
+              /main/div/div[1]/div/div[2]/div/div[1]/div/div[2]/div/div[1]/div[1]/span/div[1]/div/a'
+
+        try:
+            user_element = self.__driver.find_element(By.XPATH, xpath1)
+            user = user_element.get_attribute('href').split('/')[-2]
+            return user
+        except NoSuchElementException:
+            # If the first XPath doesn't find an element, try the second XPath
+            try:
+                user_element1 = self.__driver.find_element(By.XPATH, xpath2)
+                user1 = user_element1.get_attribute('href').split('/')[-2]
+                return user1
+            except NoSuchElementException:
+                # If neither XPath finds an element, raise an error
+                raise ValueError("Both XPath expressions failed to find an element")
+
+
+    def filter_links(self, links):
+        '''
+        Filter post links
+        '''
+        post_links = []
+        for link in links:
+            try:
+                if '.com/p/' in link.get_attribute('href'):
+                    post_links.append(link)
+            except:
+                logger.warning("A https://www.instagram.com/p/ link was not found")
+                continue
+        return post_links
+
+
+    def __get_links(self, nscrolls, scroll_pause_time):
+        '''
+        Getting posts links
+        '''
+        saved_links = {}
+        rank = 0
+        # Get scroll height
+        last_height = \
+            self.__driver.execute_script("return document.body.scrollHeight")
+
+        for j in tqdm(range(nscrolls)):
+            self.__driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            links = self.__driver.find_elements(By.TAG_NAME, 'a')
+
+            valid_links =  self.filter_links(links)
+
+            for i in range(len(valid_links)):
+                link = valid_links[i].get_attribute('href')
+                if link not in saved_links.keys():
+                    saved_links[link] = rank
+                    rank += 1
+
+            # Wait to load page
+            time.sleep(scroll_pause_time)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = \
+                self.__driver.execute_script("return document.body.scrollHeight")
+
+            if new_height == last_height:
+                # If heights are the same it will exit the function
+                break
+            last_height = new_height
+
+        return saved_links
+    
+    def __get_caption(self):
+        caption_xpath = '//div[contains(@id, "mount_0_0_")]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section\
+        /main/div/div[1]/div/div[2]/div/div[2]/div/div/div[1]/div/div[2]/div/span/div/span'    
+        caption = self.__driver.find_element(By.XPATH, caption_xpath)
+        caption = caption.get_attribute('innerHTML')
+        return caption
+
+    
+    def __get_likes(self):
+        likes_xpath = '//div[contains(@id, "mount_0_0_")]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section\
+        /main/div/div[1]/div/div[2]/div/div[3]/section/div/div/span/a/span'
+        likes_elements = self.__driver.find_elements(By.XPATH, likes_xpath)
+        likes_list = []
+
+        for likes in likes_elements:
+            likes_text = likes.get_attribute('innerHTML')
+            likes_list.append(likes_text)
+
+        return likes_list
+
+    def __get_date(self):
+        date_xpath = '//div[contains(@id, "mount_0_0_")]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section\
+        /main/div/div[1]/div/div[2]/div/div[3]/div[2]/div/a/span/time'
+        date = self.__driver.find_element(By.XPATH, date_xpath)
+        date = date.get_attribute('datetime')
+        return date
+
+    def __get_image_data(self, link):
+        '''
+        Get image data
+        '''
+        infos = {}
+        infos['user'] = self.__get_user()
+        infos['caption'] = self.__get_caption()
+        infos['likes'] = self.__get_likes()
+        infos['date'] = self.__get_date()
+        infos['link'] = link
+
+        return infos
+
+    def get_data(self, nscrolls, scroll_pause_time) -> dict:
+        '''
+        Get all hashtag data
+        '''
+        links = self.__get_links(nscrolls, scroll_pause_time)
+        logger.info(str(len(links)) + " links were found.")
+
+        processed_data = []
+        for link,rank in tqdm(links.items()):
+            infos = {}
+
+            #Accessing the post
+            self.__driver.get(link)
+            time.sleep(1)
+            try:
+                infos = self.__get_image_data(link)
+
+            except:
+                logger.warning("Failed to retrieve " + link + ' data. Skipping.')
+                time.sleep(3)
+                continue
+
+            if infos not in processed_data and infos is not None:
+                processed_data.append(infos)
+            time.sleep(1)
+
+        return processed_data
+    
+
+        
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--login', required=True,
+                                    help="Instagram login")
+
+    parser.add_argument('-p', '--password', required=True,
+                                    help="Instagram password")
+
+    parser.add_argument('-s', '--search', default='test', required=True,
+                                    help="Hashtag to be searched")
+
+    parser.add_argument('-n', '--n_scrolls', default=100, required=True,
+                                    help="Number of scrolls")
+
+    parser.add_argument('-o', '--output', default='test.csv', required=True,
+                                    help="Output filename")
+    
+
+    args = parser.parse_args()
+
+
+    my_bot = InstaScraper(
+                 username = args.login,
+                 password = args.password)
+    
+    time.sleep(5)
+    my_bot.search(args.search)
+    time.sleep(5)
+
+    data = my_bot.get_data(nscrolls = int(args.n_scrolls),
+                                    scroll_pause_time = 5)
+
+    logger.info(str(len(data)) + " links were found.")
+
+    df = pd.DataFrame(data)
+    df.to_csv(args.output, index = False)
+    logger.info("Results saved at " + args.output)
+    
+    my_scraper = ImageScraper(username = args.login,
+                 password = args.password)
+    
+    time.sleep(5)
+    my_scraper.scrape(args.search)
+    
+print("Process Completed Successfully!!!")
+    
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# NewRepo
+The given tool can extract usernames, picture captions, likes, date of upload from pictures under a Hashtag.
+To use this tool follow the below given steps:
+1. Clone the repository
+2. Open your terminal
+3. Setup a virtual environment:
+py -m virtualenv 'anyname'
+4. Install the requirements.txt file (this is a one time process) using the following syntax on your system
+py -m pip install -r requirements.txt
+5. After installation the tool is read to use. 
+py Instagram_Scraper.py -l 'your username' -p 'your password' -s 'the tag you want to scrap' -n 2 -o 'any name you want'.csv
+
+The tool will extract the data and save a csv file in the same repository or folder
+
+Beware, Instagram keeps updating their interface and that could cause this tool to not work if you end up using it. There is a quick fix and that is to update the xpaths for all the components in the code.
+It is very easy to do.
+Here is how you can update the xpaths to the files:
+Take an example of likes.
+1. Go to your desired post and click inspect/inspect element.
+2. Press ctrl+shift+c. This nnow lets you inspect specific elements.
+3. Hover over the likes, double click them and their HTML code is highlighted. You can copy this code as an xpath and update it for your use in the future.
+
+This particular code is in large parts written by https://github.com/hugozanini/hugozanini and my effort into it is minimal. The original tool that he made public is https://github.com/hugozanini/instagram-bot.
+I have updated all the parameters to work seemlessly with newer versions of selenium.
+
+CAUTION: You would be required to install python 3.9 series as there are speciic libraries in the requirements.txt that need it and are not compatible with newer versions of python.
